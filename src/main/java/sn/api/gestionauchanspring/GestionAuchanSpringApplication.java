2b87c1c22151aeb2829b03750b51edb31d6f5ef7
annandiaye11 <anna.ndiaye1@ism.edu.sn>
2025-01-18 13:45:17 +0000

    starting

---
+++ src/main/java/sn/api/gestionauchanspring/GestionAuchanSpringApplication.java
@@ -0,0 +1,13 @@
+package sn.api.gestionauchanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAuchanSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAuchanSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/api/gestionauchanspring/GestionAuchanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.api.gestionauchanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAuchanSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
